749130eb591f5071fca5f471b513db51f3597a26
omar0195-cloud <omar.alvarado@g.austincc.edu>
2025-03-08 20:13:55 +0000

    HW 2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,23 @@
-#
+
+def get_options_ratio (option, total):
+
+    ratio = option / total
+    return ratio
+
+def get_faculty_rating (ratio):
+    if 0.9 <= ratio < 1:
+        return "Excellent" 
+    
+    elif 0.8 < ratio < 0.9:
+        return "Very Good"
+    
+    elif 0.7 < ratio < 0.8:
+        return "Good"
+    
+    elif 0.6 < ratio < 0.7:
+        return "Needs Improvement"
+    
+    elif 0 < ratio <= 0.6:
+        return "Unacceptable"
+    
+
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,13 @@
-#
+#import decisions
+
+from decisions import get_options_ratio, get_faculty_rating
+
+options = float(input("Options Number: "))
+
+total = float(input("Total Number: "))
+
+result = get_options_ratio(options, total)
+
+print("Faculty Rating: ", get_faculty_rating(result))
+
+
