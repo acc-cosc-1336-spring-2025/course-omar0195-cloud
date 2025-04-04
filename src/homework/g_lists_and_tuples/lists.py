0db669df4d914751725212a97e5b074f07a57e25
omar0195-cloud <omar.alvarado@g.austincc.edu>
2025-04-04 01:37:12 +0000

    HW 5

--- src/homework/g_lists_and_tuples/lists.py
+++ src/homework/g_lists_and_tuples/lists.py
@@ -1,1 +1,20 @@
-#
+def get_lowest_list_value(input_value):
+    list = input_value
+    lowest = int(list[0])
+
+    for i in range(len(list)):
+        if lowest > int(list[i]):
+            lowest = int(list[i])
+    
+    return lowest
+
+
+def get_highest_list_value(input_value):
+    list = input_value
+    highest = int(list[0])
+
+    for i in range(len(list)):
+        if highest < int(list[i]):
+            highest = int(list[i])
+
+    return highest 
--- tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,1 +1,13 @@
-#
+import unittest
+
+from src.homework.g_lists_and_tuples.lists import get_lowest_list_value
+from src.homework.g_lists_and_tuples.lists import get_highest_list_value
+
+class Test_Config(unittest. TestCase):
+    def test_get_lowest_list_value(self):
+        list1 = [8, 10, 1, 50, 20]
+        self.assertEqual (get_lowest_list_value(list1), 1)
+
+    def test_get_highest_list_value(self):
+        list1 = [8, 10, 1, 50, 20]
+        self.assertEqual (get_highest_list_value(list1), 50)
